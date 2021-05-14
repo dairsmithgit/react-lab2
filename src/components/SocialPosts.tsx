18ecf48e7e5aeef374b887b7b958efca61c3ef3a
Dair Smith <dair.code@gmail.com>
2021-05-14 16:46:33 -0400

    imports to posts

--- src/components/SocialPosts.tsx
+++ src/components/SocialPosts.tsx
@@ -1,6 +1,8 @@
 import './SocialPosts.css';
 import { useState } from 'react';
 import Post from '../model/Post';
+import PostForm from './PostForm';
+import PostInList from './PostInList';
 
 function SocialPosts() {
 
@@ -10,11 +12,12 @@ function SocialPosts() {
         { title: "Goodnight", thought: "In case I don't see ya, good afternoon, good evening and goodnight."},
         { title: "Weird Dreams", thought: "You ever have that feeling where you're not sure if you're awake or still dreaming?"},
         { title: "Doctor Visit", thought: "I really need to go to the doctor to have my leg checked."},
-    ])
+    ]);
 
     return (
-        <div>
-
+        <div className="SocialPosts">
+            <h1>Thinker Feed </h1>
+            <h2>Social media for people with big brains.</h2>
         </div>
     )
 }
