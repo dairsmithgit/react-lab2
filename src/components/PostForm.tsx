a84fb3386142639cebcee677266bbddab036df4d
Dair Smith <dair.code@gmail.com>
2021-05-14 16:06:18 -0400

    more setup for child comps

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -1,2 +1,16 @@
 import './PostForm.css';
 import { useState } from 'react';
+import Post from '../model/Post';
+
+function PostForm() {
+
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PostForm;
--- src/components/PostInList.tsx
+++ src/components/PostInList.tsx
@@ -1,2 +1,16 @@
 import './PostInList.css';
 import { useState } from 'react';
+import Post from '../model/Post';
+
+function PostInList() {
+
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PostInList;
