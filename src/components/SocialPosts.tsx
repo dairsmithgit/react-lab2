a98c4e5851e4133d6f846bd6f8f7a5c07f2b1eb6
Dair Smith <dair.code@gmail.com>
2021-05-14 15:48:04 -0400

    started social posts

--- src/components/SocialPosts.tsx
+++ src/components/SocialPosts.tsx
@@ -1,2 +1,20 @@
 import './SocialPosts.css';
 import { useState } from 'react';
+import Post from '../model/Post';
+
+function SocialPosts() {
+
+    const [ newPost, setNewPost ] = useState<Post[]>([
+        { title: "Question", thought: "What's the most you've ever lost on a coin toss?"},
+        { title: "Mishap", thought: "Aw, man. I shot Marvin in the face."},
+        { title: "Goodnight", thought: "In case I don't see ya, good afternoon, good evening and goodnight."},
+        { title: "Weird Dreams", thought: "You ever have that feeling where you're not sure if you're awake or still dreaming?"},
+        { title: "Doctor Visit", thought: "I really need to go to the doctor to have my leg checked."},
+    ])
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
